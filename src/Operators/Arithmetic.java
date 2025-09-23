b2f3a6ac7374fa8e6fe7a8e62c8bf2e4fc1bf191
shadow-eljan <eljanrai25@gmail.com>
2025-09-23 13:49:05 +0545

    order of P-E-M-D-A-S

---
+++ src/Operators/Arithmetic.java
@@ -0,0 +1,21 @@
+package Operators;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class Arithmetic {
+    public static void main(String[] args) {
+
+        double x = 33;
+        double y = 22;
+        double z = 0;
+
+//        z = x + y;
+//        z = x - y;
+//        z = x * y;
+//        z = x / y;
+//        z = x % y;
+
+        System.out.println(z);
+    }
+
+}
---
+++ src/Operators/PEMDAS.java
@@ -0,0 +1,12 @@
+package Operators;
+
+public class PEMDAS {
+    public static void main(String[] args) {
+
+        //ORDER OF P-E-M-D-A-S
+
+        double result = 3 + 4 * 2 / 2.0- 4.00;
+
+        System.out.println(result);
+    }
+}
