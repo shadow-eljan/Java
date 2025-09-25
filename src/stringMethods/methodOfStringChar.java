0259602421f642798c807b2715e2a7934c46a0f4
shadow-eljan <eljanrai25@gmail.com>
2025-09-25 20:44:08 +0545

    Substring method

--- src/stringMethods/methodOfStringChar.java
+++ src/stringMethods/methodOfStringChar.java
@@ -1,3 +1,5 @@
+package stringMethods;
+
 public class methodOfStringChar {
     public static void main(String[] args) {
 
---
+++ src/stringMethods/subString.java
@@ -0,0 +1,30 @@
+package stringMethods;
+
+import java.util.Scanner;
+
+public class subString {
+    public static void main(String[] args) {
+
+        //.substring = a method used to extract a portion of a string
+        // string.substring(start, end)
+
+        Scanner scanner = new Scanner(System.in);
+
+        String email;
+
+        System.out.print("Enter your gmail: ");
+        email = scanner.nextLine();
+
+        if(email.contains("@")){
+            String username = email.substring(0, email.indexOf("@"));
+            String domain = email.substring(email.indexOf("@")+1);
+
+            System.out.println("Username: "+ username);
+            System.out.println("Domain: " + domain);
+        }else{
+            System.out.println("Email must contain @ char.");
+        }
+
+
+    }
+}
