c6a69c795308b73f6a081c588d48d408dd1fca66
shadow-eljan <eljanrai25@gmail.com>
2025-11-13 22:35:34 +0545

    Exception Practise

---
+++ src/exceptions/arithmaticException.java
@@ -0,0 +1,13 @@
+package exceptions;
+
+public class arithmaticException {
+        public static void main(String[] args) {
+            try {
+                int result = 10 / 0; // This will cause ArithmeticException
+                System.out.println("Result: " + result);
+            } catch (ArithmeticException e) {
+                System.out.println("Error: Cannot divide by zero!");
+            }
+            System.out.println("Program continues...");
+        }
+}
