20e03c3c790e2773833a63409fc74297a8f6da48
shadow-eljan <eljanrai25@gmail.com>
2025-11-14 20:06:07 +0545

    Generics practise

---
+++ src/Generics/practise1.java
@@ -0,0 +1,20 @@
+package Generics;
+
+public class practise1<T> {
+
+        // create a generics method
+        public <T> void display(T data) {
+
+            System.out.println("Data: " + data);
+        }
+
+        public static void main(String[] args) {
+            practise1 obj = new practise1();
+
+            // generics method working with String
+            obj.<String>display("Java");
+
+            // generics method working with integer
+            obj.<Integer>display(25);
+        }
+}
---
+++ src/Generics/practise2.java
@@ -0,0 +1,4 @@
+package Generics;
+
+public class practise2 {
+}
